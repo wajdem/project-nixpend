b52e98e4f28a66aecfc40470445caec448dc2f1b
wajdem <wajdi.abuarqob@gmail.com>
2023-08-09 20:51:34 +0300

    done package json and express

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,16 @@
 const express = require("express")
+
+
+// express app
+const app = express()
+
+//routes  
+app.get('/', (req, res) =>{
+    res.json({mssg:"Welcome to the app"})
+
+})
+
+//listen for requests
+app.listen(8686, ()=>{
+    console.log("listening on port 8686!!!");
+})
