7381f4863920a3620673fad4d6ecd0a16d3817b6
wajdem <wajdi.abuarqob@gmail.com>
2023-08-09 21:54:26 +0300

    done headers

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
+
 const express = require("express")
 
 
 // express app
 const app = express()
+
+app.use((req, res, next) =>{
+    console.log(req.path, res.method);
+    next()
+})
 
 //routes  
 app.get('/', (req, res) =>{
@@ -11,6 +18,8 @@ app.get('/', (req, res) =>{
 })
 
 //listen for requests
-app.listen(8686, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log("listening on port 8686!!!");
 })
+
+process.env
