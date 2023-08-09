494d828daace5d603cc201d8efe052b854919024
wajdem <wajdi.abuarqob@gmail.com>
2023-08-09 22:06:58 +0300

    done Form

--- frount-end/my-react-app/src/App.js
+++ frount-end/my-react-app/src/App.js
@@ -1,10 +1,13 @@
+import './App.css'
 import React from 'react'
 import Header from './components/Header'
+import ToDoForm from './components/ToDoForm'
 
 function App() {
   return (
     <div>
       <Header />
+      <ToDoForm/>
     </div>
   )
 }
---
+++ frount-end/my-react-app/src/components/ToDoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ToDoForm() {
+  return (
+    <>
+    <form>
+        <input />
+    </form>
+      
+    </>
+  )
+}
+
+export default ToDoForm
